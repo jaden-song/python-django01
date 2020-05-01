7f8f981a14b44066079e314b68f4f136918c7ce0
mingu-song <songmingu@gmail.com>
2020-05-01 21:11:11 +0900

    두번째 커밋

--- django01/settings.py
+++ django01/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ux!275()mas%pgo+yt!xd^j4w@ji4m6f5qk5oha68^-lc&0r)7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
